a27ac751a180050ba7640a2d486d6a138d46d426
arbyun <someonealiveh@gmail.com>
2023-05-19 10:34:45 +0100

    copy previous program's classes

---
+++ PlayerManager4/Player.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace PlayerManager4
+{
+    public class Player: IComparable<Player>
+    {
+        public string Name { get; }
+        public int Score { get; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public int CompareTo(Player other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            var nameComparison = string.Compare(Name, other.Name, StringComparison.Ordinal);
+            if (nameComparison != 0) return nameComparison;
+            return other.Score.CompareTo(Score);
+        }
+    }
+}
