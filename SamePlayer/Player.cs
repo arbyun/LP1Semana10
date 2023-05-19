aef1ffd3a865d65acb6b2b59ec1bff515841dee3
arbyun <someonealiveh@gmail.com>
2023-05-19 11:17:18 +0100

    override Equals() to determine if player is unique

--- SamePlayer/Player.cs
+++ SamePlayer/Player.cs
@@ -10,5 +10,14 @@
             // XOR (^) the hash codes of the Type and Name fields
             return Type.GetHashCode() ^ Name.GetHashCode();
         }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Player otherPlayer = (Player)obj;
+            return Type == otherPlayer.Type && Name == otherPlayer.Name;
+        }
     }
 }
