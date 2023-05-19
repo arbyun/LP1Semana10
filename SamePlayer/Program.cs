b064f1bb708f24a8edc1d210968b483106961b4f
arbyun <someonealiveh@gmail.com>
2023-05-19 11:18:19 +0100

    create players and hashset setOfPlayers

--- SamePlayer/Program.cs
+++ SamePlayer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SamePlayer
 {
@@ -6,7 +7,16 @@ namespace SamePlayer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HashSet<Player> setOfPlayers = new HashSet<Player>();
+
+            Player player1 = new Player { Type = PlayerType.Tank, Name = "Ana" };
+            Player player2 = new Player { Type = PlayerType.Slayer, Name = "Paulo" };
+            Player player3 = new Player { Type = PlayerType.Tank, Name = "Ana" };
+
+            setOfPlayers.Add(player1);
+            setOfPlayers.Add(player2);
+            setOfPlayers.Add(player3);
         }
     }
 }
+
