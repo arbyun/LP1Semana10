fc9b64104bd4d8a0cb1b1824cdf45bab48e361df
arbyun <someonealiveh@gmail.com>
2023-05-19 11:40:11 +0100

    implement IComparable<Loot> to class Loot

--- LootSort/Loot.cs
+++ LootSort/Loot.cs
@@ -1,26 +1,13 @@
+using System;
+
 namespace LootSort
 {
-    /// <summary>
-    /// The Loot class should implement IComparable<Loot>
-    /// and override GetHashCode() and Equals()
-    /// </summary>
-    public class Loot
+    public class Loot: IComparable<Loot>
     {
-        /// <summary>Type of loot.</summary>
         public LootType Kind { get; }
-
-        /// <summary>Loot description.</summary>
         public string Description { get; }
-
-        /// <summary>Loot value.</summary>
         public float Value { get; }
-
-        /// <summary>
-        /// Create a new instance of loot.
-        /// </summary>
-        /// <param name="kind">Type of loot.</param>
-        /// <param name="description">Loot description.</param>
-        /// <param name="value">Loot value.</param>
+        
         public Loot(LootType kind, string description, float value)
         {
             Kind = kind;
@@ -28,13 +15,24 @@ namespace LootSort
             Value = value;
         }
 
-        /// <summary>
-        /// Return a nicely formatted string representing the loot instance.
-        /// </summary>
-        /// <returns>
-        /// A nicely formatted string representing the loot instance.
-        /// </returns>
         public override string ToString() =>
             $"[{Kind,15}]\t{Value:f2}\t{Description}";
+        
+        public int CompareTo(Loot other)
+        {
+            int typeComparison = String.Compare(Kind.ToString(), other.Kind.ToString(), StringComparison.Ordinal);
+            if (typeComparison != 0)
+            {
+                return typeComparison;
+            }
+
+            int valueComparison = -Value.CompareTo(other.Value);
+            if (valueComparison != 0)
+            {
+                return valueComparison;
+            }
+
+            return String.Compare(Description, other.Description, StringComparison.Ordinal);
+        }
     }
 }
