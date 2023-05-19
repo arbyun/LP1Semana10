c33d20605fbddb2dc5484f76b299f6191c6843b4
arbyun <someonealiveh@gmail.com>
2023-05-19 10:42:35 +0100

    create bool with class constructor

--- PlayerManager4/CompareByName.cs
+++ PlayerManager4/CompareByName.cs
@@ -5,5 +5,11 @@ namespace PlayerManager4
 {
     public class CompareByName : IComparer<Player>
     {
+        private readonly bool _ascending;
+
+        public CompareByName(bool ascending)
+        {
+            this._ascending = ascending;
+        }
     }
 }
